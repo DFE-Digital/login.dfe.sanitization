1657ef4b702f8997e4ca4c595000395b355b68a4
Richard Ward <richard.ward@methods.co.uk>
2025-02-17 15:27:35 +0000

    Update tests

--- test/Middleware.DefaultOpts.test.js
+++ test/Middleware.DefaultOpts.test.js
@@ -2,22 +2,26 @@ const sanitization = require("./../lib");
 const middleware = sanitization();
 
 describe("When using default options", () => {
-  const req = {
-    query: {
-      bad: "<script>alert(1);</script>",
-      good: "test",
-      mixed: "te<script>alert(1);</script>st",
-    },
-    body: {
-      bad: "<script>alert(1);</script>",
-      good: "test",
-      mixed: "te<script>alert(1);</script>st",
-    },
-  };
+  let req;
   const res = {};
   const next = jest.fn();
 
   beforeEach(() => {
+    req = {
+      query: {
+        bad: "<script>alert(1);</script>",
+        good: "test",
+        mixed: "te<script>alert(1);</script>st",
+      },
+      body: {
+        bad: "<script>alert(1);</script>",
+        good: "test",
+        mixed: "te<script>alert(1);</script>st",
+        goodJson: { id: 1, value: "test" },
+        badJson: { id: 1, value: "<script>alert(1);</script>" },
+        mixedJson: { id: 1, value: "te<script>alert(1);</script>st" },
+      },
+    };
     next.mockReset();
   });
 
@@ -55,6 +59,27 @@ describe("When using default options", () => {
     middleware(req, res, next);
 
     expect(req.body.mixed).toBe("test");
+  });
+
+  it("then it sanitize purely bad json body params", () => {
+    middleware(req, res, next);
+
+    expect(req.body.badJson.value).toBe("");
+    expect(req.body.badJson.id).toBe(1);
+  });
+
+  it("then it leave purely good json body params untouched", () => {
+    middleware(req, res, next);
+
+    expect(req.body.goodJson.value).toBe("test");
+    expect(req.body.goodJson.id).toBe(1);
+  });
+
+  it("then it sanitize json body params with bad mixed in good", () => {
+    middleware(req, res, next);
+
+    expect(req.body.mixedJson.value).toBe("test");
+    expect(req.body.mixedJson.id).toBe(1);
   });
 
   it("then it should call next", () => {
